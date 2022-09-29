67c029aa3a34662486c017b48888834ca689bf8e
mthomas41 <mthomas41@huskers.unl.edu>
2022-09-29 16:53:11 -0500

    slides will now display cards

--- frontend/src/app/banner/banner.page.ts
+++ frontend/src/app/banner/banner.page.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import SwiperCore, { EffectFade, Swiper } from 'swiper';
 
 // install Swiper modules
-SwiperCore.use([EffectFade]);
+//SwiperCore.use([EffectFade]);
 
 @Component({
   selector: 'app-banner',
@@ -10,5 +10,16 @@ SwiperCore.use([EffectFade]);
   styleUrls: ['banner.page.scss'],
 })
 export class BannerPage {
+  bannerMessages: { id: number, name: string }[] = [
+    { "id": 0, "name": "Available" },
+    { "id": 1, "name": "Ready" },
+    { "id": 2, "name": "Started" }
+];
+  //bannerMessages: Array<string> = ['Apple', 'Orange', 'Banana'];
+  //bannerMessages = []; 
   constructor() {}
+  // ngOnInit() {
+  //fetch from data base.
+  // }
+  
 }
