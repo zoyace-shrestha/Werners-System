17ceab949d3c5748483eee0d5e6fa9387488b49c
zshrestha2 <99230624+zoyace-shrestha@users.noreply.github.com>
2022-10-04 18:17:43 -0500

    Card component modified

--- frontend/src/app/banner/banner.page.ts
+++ frontend/src/app/banner/banner.page.ts
@@ -6,23 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['banner.page.scss'],
 })
 export class BannerPage {
-
   bannerMessages: {
     id: number;
     title: string;
+    subtitle: string;
     description: string;
     backgroundColor: string;
+    date: Date;
   }[] = new Array();
 
   public parseJson(
-    json: { id: number; title: string; description: string; backgroundColor: string }[]
+    json: {
+      id: number;
+      title: string;
+      subtitle: string;
+      description: string;
+      backgroundColor: string;
+      date: Date;
+    }[]
   ) {
     json.forEach((x) => {
       this.bannerMessages.push({
         id: x.id,
         title: x.title,
+        subtitle: x.subtitle,
         description: x.description,
         backgroundColor: x.backgroundColor,
+        date: x.date,
       });
     });
   }
@@ -30,40 +40,71 @@ export class BannerPage {
   constructor() {}
 
   ngOnInit() {
-
     setTimeout(() => {
       let sampleData = [
         {
           id: 0,
-          title: 'Banner 1',
-          description: 'Description for banner 1',
-          backgroundColor: backgroundColors.primary,
+          title: 'Werner News ',
+          subtitle: '2022 June Associate Recognition Ceremony',
+          description:
+            'Werner was pleased to recognize four professional drivers: David C, Robert L, Daniel P, and Raymond W. Werner was pleased to recognize four professional drivers: David C, Robw ',
+          backgroundColor: backgroundColors.tertiary,
+          date: new Date(),
         },
         {
           id: 1,
-          title: 'Banner 2',
-          description: 'Description for banner 2',
+          title: 'Werner Life',
+          subtitle: 'Life on the Road Video Series',
+          description:
+            'The latest episode of Life on the Road is available now!',
           backgroundColor: backgroundColors.secondary,
+          date: new Date(),
         },
         {
           id: 2,
-          title: 'Banner 3',
-          description: 'Description for banner 3',
-          backgroundColor: backgroundColors.tertiary,
+          title: 'COVID-19 Updates',
+          subtitle: 'What Drivers Need to Know About COVID-19',
+          description: 'The latest information about COVID- 19',
+          backgroundColor: backgroundColors.secondary,
+          date: new Date(),
+        },
+        {
+          id: 3,
+          title: 'Werner Points',
+          subtitle: 'Werner rewards has been added!',
+          description: 'Click here to get started! ',
+          backgroundColor: backgroundColors.success,
+          date: new Date(),
+        },
+        {
+          id: 4,
+          title: 'COVID-19 Updates',
+          subtitle: 'What Drivers Need to Know About COVID-19',
+          description: 'The latest information about COVID-19  ',
+          backgroundColor: backgroundColors.danger,
+          date: new Date(),
+        },
+        {
+          id: 5,
+          title: 'Get Paid ',
+          subtitle: '2022 June Associate Recognition Ceremony',
+          description:
+            'Werner was pleased to recognize four professional drivers: David C, Robert L, Daniel P, and Raymond W. Werner was pleased to recognize four professional drivers: David C, Robw',
+          backgroundColor: backgroundColors.success,
+          date: new Date(),
         },
       ];
       this.parseJson(sampleData);
     }, 1000);
-
   }
 }
 
-const backgroundColors = 
-{
+const backgroundColors = {
   white: '#FFFFFF',
-  green: '#218719',
-  red: '#C03131',
+  success: '#218719',
+  danger: '#C03131',
+  warning: '#FFFFFF',
   tertiary: '#163762',
   secondary: '#1877F2',
   primary: '#3369B4',
-}
+};
