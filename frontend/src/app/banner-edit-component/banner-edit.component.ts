5d2e688fa906e756f3f0de608ae5547195c52306
P Poling <ppoling@huskers.unl.edu>
2022-10-11 18:49:36 -0500

    for loop on banner page

--- frontend/src/app/banner-edit-component/banner-edit.component.ts
+++ frontend/src/app/banner-edit-component/banner-edit.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit } from '@angular/core';
+import getTestBanners from '../../services/banner';
 
 
 @Component({
@@ -8,11 +9,47 @@ import {Component, OnInit } from '@angular/core';
 })
 export class BannerEditComponent implements OnInit {
   
+  bannerMessages: {
+    id: number;
+    title: string;
+    subtitle: string;
+    description: string;
+    backgroundColor: string;
+    textColor: string;
+    date: Date;
+  }[] = new Array();
 
+  public async parseJson(
+    json: {
+      id: number,
+      title: string,
+      subtitle: string,
+      description: string,
+      colorScheme: {
+        background: string,
+        text: string
+      },
+      date: Date
+    }[]
+  ) {
+    json.forEach((x) => {
+      this.bannerMessages.push({
+        id: x.id,
+        title: x.title,
+        subtitle: x.subtitle,
+        description: x.description,
+        backgroundColor: x.colorScheme.background,
+        textColor: x.colorScheme.text,
+        date: x.date,
+      });
+    });
+  }
   constructor() {}
 
   ngOnInit() {
-   
+    getTestBanners().then((json) => {
+      this.parseJson(json);
+    })
   }
 }
 
