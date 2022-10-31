db6ec5e2602a0e6d0f2dc19219fa65179f395b28
AndresRuiz01 <andres.ruiz3210@gmail.com>
2022-10-30 21:21:53 -0500

    UI updates

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -9,11 +9,11 @@ export class HomeComponent {
 
   ann: Announcement = {
     id: 1,
-    title: "2022 June Associate Recognition Ceremony",
-    description: "Werner was pleased to recognize four professional drivers: David C, Robert L, Daniel P, and Raymond W. Werner was pleased to recognize four professional drivers: David C, Robw",
-    type: "Werner News",
-    link: "Link",
-    background: "primary",
+    type: 'Werner Life',
+    title: 'Life on the Road Video Series',
+    description: 'The latest episode of Life on the Road is available now!',
+    background: 'secondary',
+    link: '',
     publishDate: new Date(),
     expirationDate: new Date(),
     isDraft: false
