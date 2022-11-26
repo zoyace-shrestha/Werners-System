84e04cb232a7c34026ac289725f159bed04e4573
thomaskmatthew <61100740+thomaskmatthew@users.noreply.github.com>
2022-11-26 14:29:43 -0600

    feature/announcement_link (#63)
    * Update announcement-card.component.ts
    
    * Update announcement-card.component.html
    
    * updated dendencies for the in-app-browser
    
    * Update announcement-card.component.css
    
    * Update announcement-card.component.html
    
    * Update announcement-card.component.html
    
    * Update announcement-card.component.html
    
    Co-authored-by: mthomas41 <mthomas41@huskers.unl.edu>
    Co-authored-by: Andres Ruiz <46800929+AndresRuiz01@users.noreply.github.com>

--- ClientApp/src/app/announcement-card/announcement-card.component.ts
+++ ClientApp/src/app/announcement-card/announcement-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Announcement } from '../announcement';
+import { InAppBrowser , InAppBrowserOptions } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'app-announcement-card',
@@ -9,8 +10,30 @@ import { Announcement } from '../announcement';
 export class AnnouncementCardComponent implements OnInit {
 
   @Input() announcement? : Announcement;
+  theInAppBrowser: typeof InAppBrowser = InAppBrowser;
+   options : InAppBrowserOptions = {
+    location : 'yes',//Or 'no' 
+    hidden : 'no', //Or  'yes'
+    clearcache : 'yes',
+    clearsessioncache : 'yes',
+    zoom : 'yes',//Android only ,shows browser zoom controls 
+    hardwareback : 'yes',
+    mediaPlaybackRequiresUserAction : 'no',
+    shouldPauseOnSuspend : 'no', //Android only 
+    closebuttoncaption : 'Close', //iOS only
+    disallowoverscroll : 'no', //iOS only 
+    toolbar : 'yes', //iOS only 
+    enableViewportScale : 'no', //iOS only 
+    allowInlineMediaPlayback : 'no',//iOS only 
+    presentationstyle : 'pagesheet',//iOS only 
+    fullscreen : 'yes', //Windows only    
+    };
 
   constructor() { }
+  public openWithSystemBrowser(url : string){
+    const target = "_system";
+    this.theInAppBrowser.create(url,target,this.options);
+  }
 
   ngOnInit(): void {  }
   dateFormat(dateString: string) {
