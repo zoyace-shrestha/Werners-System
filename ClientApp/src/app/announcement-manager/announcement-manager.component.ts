3678424bcf04cd0cd4b7c130d6f87a3597238b24
Andres Ruiz <46800929+AndresRuiz01@users.noreply.github.com>
2022-11-26 14:01:36 -0600

    Delete Fucntionality Added (#73)
    Co-authored-by: zshrestha2 <99230624+zoyace-shrestha@users.noreply.github.com>

--- ClientApp/src/app/announcement-manager/announcement-manager.component.ts
+++ ClientApp/src/app/announcement-manager/announcement-manager.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { result } from 'lodash';
 import { Announcement } from '../announcement';
 import { BannerService } from '../banner.service';
 
@@ -11,13 +12,21 @@ export class AnnouncementManagerComponent implements OnInit {
 
   announcements: Array<Announcement> = [];
 
-  constructor(private bannerService: BannerService) {}
+  constructor(private bannerService: BannerService) { }
 
   dateFormat(dateString: string) {
     let date = new Date(dateString);
-    const options: Intl.DateTimeFormatOptions = {weekday: undefined, year: 'numeric', month: 'short', day: 'numeric' };
+    const options: Intl.DateTimeFormatOptions = { weekday: undefined, year: 'numeric', month: 'short', day: 'numeric' };
     return date.toLocaleDateString(undefined, options);
-  } 
+  }
+
+  onDelete(banner: Announcement) {
+    this.bannerService.deleteById(banner.idAnnoucements).subscribe(result => { }, error => console.log(error));
+    const index = this.announcements.indexOf(banner);
+    if (index > -1) {
+      this.announcements.splice(index, 1);
+    }
+  }
 
   ngOnInit() {
     // Retrieve active banners
@@ -35,4 +44,5 @@ export class AnnouncementManagerComponent implements OnInit {
       loadingComponent.style.display = 'none'
     }
   }
+
 }
